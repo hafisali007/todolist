369c0bbcfe3ae30594d5285c5b28369cf0e1522e
Hafis Ali <hafisali007@gmail.com>
2021-05-31 00:16:19 +0400

    Updated

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 
 
 function App() {
-   const [toDo, setToDo] = useState([]);
-   const [toDos, setToDos] = useState('');
+   const [toDo, setToDo] = useState('');
+   const [toDos, setToDos] = useState([]);
   return (
 <div className="container m-5 p-2 rounded mx-auto bg-light shadow">
    
@@ -26,39 +26,51 @@ function App() {
                 </div>
               
                 <div className="col-auto px-0 mx-0 mr-2">
-                    <button type="button" className="btn btn-primary">Add</button>
+                    <button type="button" onClick={(e) => setToDos([...toDos,{text:toDo, status:false}])} className="btn btn-primary">Add</button>
+                    {/* SpreadOperator */}
                 </div>
             </div>
         </div>
     </div>
     <div className="p-2 mx-4 border-black-25 border-bottom"></div>
-
+    
+{ 
+toDos.map((obj) => {
+return (
     <div className="row mx-1 px-5 pb-3 w-80">
-        <div className="col mx-auto">
-            <div className="row px-3 align-items-center todo-item rounded">
-                <div className="col-auto m-1 p-0 d-flex align-items-center">
-                    <h2 className="m-0 p-0">
-                        {/* <i className="fa fa-square-o text-primary btn m-0 p-0 d-none" data-toggle="tooltip" data-placement="bottom" title="Mark as complete"></i> */}
-                        <i className="fa fa-check-square-o text-primary btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Mark as todo"></i>
-                    </h2>
-                </div>
-                <div className="col px-1 m-1 d-flex align-items-center">
-                    {/* <input type="text" className="form-control form-control-lg border-0 edit-todo-input bg-transparent rounded px-3" readonly value="Buy groceries for next week" title="Buy groceries for next week" /> */}
-                    <input type="text" className="form-control form-control-lg border-0 edit-todo-input rounded px-3 d-none" value="Buy groceries for next week" />
-                </div>
-                <div className="col-auto m-1 p-0 px-3 d-none">
-                </div>
-                <div className="col-auto m-1 p-0 todo-actions">
-                    <div className="row d-flex align-items-center justify-content-end">                      
-                        <h5 className="m-0 p-0 px-2">
-                            <i className="fa fa-trash-o text-danger btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Delete todo"></i>
-                        </h5>
-                    </div>  
-                </div>
-            </div>         
-          
-        </div>
+    <div className="col mx-auto">
+        <div className="row px-3 align-items-center todo-item rounded">
+            <div className="col-auto m-1 p-0 d-flex align-items-center">
+                <h2 className="m-0 p-0">
+                    {/* <i className="fa fa-square-o text-primary btn m-0 p-0 d-none" data-toggle="tooltip" data-placement="bottom" title="Mark as complete"></i> */}
+                   {/* <i className="fa fa-square-o text-primary btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Mark as todo"></i>  */}
+                    {/* <input className="fa fa-square-o text-primary btn m-0 p-0" type="checkbox"  name="" id=""/> */}
+                    <button type="submit" id="completed-task" style={{  background: 'none', padding: '0px',border: 'none'}}>
+      <i className="fa fa-square-o text-primary btn m-0 p-0" ></i>
+</button>
+                </h2>
+            </div>
+            <div className="col px-1 m-1 d-flex align-items-center">
+                <input type="text" className="form-control form-control-lg border-0 edit-todo-input bg-transparent rounded px-3" readonly value={obj.text}/>
+                {/* <input type="text" className="form-control form-control-lg border-0 edit-todo-input rounded px-3 d-none" value={value} /> */}
+            </div>
+            <div className="col-auto m-1 p-0 px-3 d-none">
+            </div>
+            <div className="col-auto m-1 p-0 todo-actions">
+                <div className="row d-flex align-items-center justify-content-end">                      
+                    <h5 className="m-0 p-0 px-2">
+                        <i className="fa fa-trash-o text-danger btn m-0 p-0" data-toggle="tooltip" data-placement="bottom" title="Delete todo"></i>
+                    </h5>
+                </div>  
+            </div>
+        </div>         
+      
     </div>
+</div>
+)
+})
+}
+
 </div>
 );
 }
